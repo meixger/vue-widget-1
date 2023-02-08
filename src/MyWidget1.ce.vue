e29c2970e5d8a8661958d217c12dfb1183e079c6
Martin Meixger <mail@meixger.com>
2023-02-08 16:45:21 +0100

    WIP inject html attrs as props

--- src/MyWidget1.ce.vue
+++ src/MyWidget1.ce.vue
@@ -1,13 +1,29 @@
 <script setup>
+// import { computed, getCurrentInstance, onMounted, onUnmounted, ref } from "vue";
 import Counter from "./Counter.vue";
+const props = defineProps({
+  test222: String
+})
+
+// const root = ref(null)
+// // const cid = computed(() => root.value?.parentNode.dataset.cid)
+// onMounted(() => {
+//   try {
+//     console.log("onMounted dev ", root.value?.parentNode, props.test222, root.value?.parentNode?.dataset?.cid)
+//     console.log("onMounted prod", root.value, props.test222)
+//     // const cid = root.value?.parentNode.dataset.cid
+//   } catch (error) {
+//     console.log("onMounted error", error)
+//   }
+// })
 </script>
 
 <template>
-  <div id="myWidget1">
+  <div id="myWidget1" ref="root">
     <h1 class="title">My Widget 1</h1>
-    <button
-      class="rounded-full p-2 bg-red-500 border border-red-700 mb-2 color-red-300 font-bold hover:bg-red-600"
-    >
+    <!-- <pre>cid: {{ cid }}</pre> -->
+    <pre>test222: {{ test222 }}</pre>
+    <button class="rounded-full p-2 bg-red-500 border border-red-700 mb-2 color-red-300 font-bold hover:bg-red-600">
       TailWind CSS
     </button>
     <Counter />
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-import { defineCustomElement } from 'vue'
-import { createApp } from 'vue'
-import MyWidget1 from './MyWidget1.ce.vue'
-import { STYLE_PLACEHOLDER } from '../@mxg/vite-plugin-style-injector-shadowdom'
-import './main.css'
+import { defineCustomElement } from "vue";
+import { createApp } from "vue";
+import MyWidget1 from "./MyWidget1.ce.vue";
+import { STYLE_PLACEHOLDER } from "../@mxg/vite-plugin-style-injector-shadowdom";
+import "./main.css";
 
-const isCustomElementStyleAvailable = MyWidget1.styles
+const isCustomElementStyleAvailable = MyWidget1.styles;
 if (isCustomElementStyleAvailable)
-    customElements.define('my-widget-1', defineCustomElement({ ...MyWidget1, styles: [STYLE_PLACEHOLDER] }))
-else
-    createApp(MyWidget1).mount('my-widget-1')
+  customElements.define(
+    "my-widget-1",
+    defineCustomElement({ ...MyWidget1, styles: [STYLE_PLACEHOLDER] })
+  );
+else createApp(MyWidget1, { test222: "333" }).mount("#app");
